c201a29528c2892dcde5d4c5672f6f4d8fe7205b
salil <salilpa007@gmail.com>
2014-07-11 19:34:25 +0530

    Changing all classes to train search

--- train_schedule_fetch.py
+++ train_schedule_fetch.py
@@ -1,7 +1,7 @@
 import train_schedule
 
-train = train_schedule.TrainSchedule("22646", "10", "2")#month and day are highly recommended
-if train.request() == True:
+train = train_schedule.TrainSearch("12345")#month and day are highly recommended
+if train.request():
     print train.get_json()
 else:
     print train.error
